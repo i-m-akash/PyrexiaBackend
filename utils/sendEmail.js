1c8950a8c8e74af9b11e8e63aa190263a8045900
Akash kumar <109230614+i-m-akash@users.noreply.github.com>
2024-09-27 16:39:29 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,38 @@
-const nodemailer =require("nodemailer");
 
-const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: 465,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+const nodemailer = require('nodemailer');
 
-  const options = {
-    from: sent_from,
-    to: send_to,
-    replyTo: reply_to,
-    subject: subject,
-    html: message,
-  };
 
-  // Send Email
+ const sendEmail = async (subject, message, send_to, sent_from, reply_to,pdfPath) => {
+
+    // Create a Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+          host: process.env.EMAIL_HOST,
+          port: 465,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        });
+
+    // Send the email
+    const options = {
+          from: sent_from,
+          to: send_to,
+          replyTo: reply_to,
+          subject: subject,
+          html: message,
+          attachments: [
+            {
+                filename: `receipt-${send_to}.pdf`,
+                path: pdfPath,
+            },
+          ],
+
+        };
+       // Send Email
   transporter.sendMail(options, function (err, info) {
     if (err) {
       console.log(err);
@@ -29,5 +40,6 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
       console.log(info);
     }
   });
-};
+}
+
 module.exports =sendEmail;
